04b8ec47859d1aac969afc3560740d2e0061fab2
LittttleRed <2917781292@qq.com>
2024-11-06 20:25:19 +0800

    hello

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -28,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main11);
+        setContentView(R.layout.main_alarm);
         // 创建一个MaterialDatePicker实例
         ListView a=(ListView)findViewById(R.id.list_test);
         listItems=new ArrayList<Map<String, Object>>();
