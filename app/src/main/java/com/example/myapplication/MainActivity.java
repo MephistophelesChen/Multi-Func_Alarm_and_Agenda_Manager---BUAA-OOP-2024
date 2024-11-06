0e1ec438d231f28948900cb4d4838d68f3848c67
LittttleRed <2917781292@qq.com>
2024-11-06 19:53:55 +0800

    hello

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,40 +1,57 @@
 package com.example.myapplication;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    List<Map<String,Object>> listItems;
+    HashMap<String,Object> map;
+    SimpleAdapter simpleAdapter;
+    private String[] time = new String[]{"11:00","10:00","11:58","11:34"};
+    private String[] time_1=new String[]{"AM","PM","AM","PM"};
 
     private static final String TAG = "MainActivity";
-
+    private ImageButton imbtn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main11);
         // 创建一个MaterialDatePicker实例
-        ArrayList<String> ring_content= new ArrayList<>();
-        String a="1231233";
-        ring_content.add("闹钟1_____time1___________________switch_button1");
-        ring_content.add("闹钟2_____time2___________________switch_button2");
-        ring_content.add("闹钟3_____time3___________________switch_button3");
-        ring_content.add("闹钟4_____time4___________________switch_button4");   ring_content.add("闹钟3_____time3___________________switch_button3");
-        ring_content.add("闹钟3_____time3___________________switch_button3");
-        ring_content.add("闹钟3_____time3___________________switch_button3");
-        ring_content.add("闹钟3_____time3___________________switch_button3");
-        ring_content.add("闹钟3_____time3___________________switch_button3");
-        ring_content.add("闹钟3_____time3___________________switch_button3");
-        ring_content.add("......");
+        ListView a=(ListView)findViewById(R.id.list_test);
+        listItems=new ArrayList<Map<String, Object>>();
+        for(int i=0;i<time.length;i++)
+        {
+            map=new HashMap<String, Object>();
+            map.put("time", time[i]);
+            map.put("time_1", time_1[i]);
+            //把列表项加进列表集合
+            listItems.add(map);
+        }
+     //  simpleAdapter =new SimpleAdapter(this,listItems,R.layout.data_list_res,new String[]{"time","time_1"},new int[]);
+      //  a.setAdapter(simpleAdapter);
 
-        ArrayAdapter<String> adapter=new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_list_item_1,ring_content);
-        ListView listView=(ListView)findViewById(R.id.list_test);
-        listView.setAdapter(adapter);
-        adapter.insert(a,3);
+        imbtn1=(ImageButton) findViewById(R.id.add_alarm_btn);
+        imbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, create_alarm_data_activity.class);
+                // 启动Activity2
+                startActivity(intent);
+            }
+        });
     }
 //我们需要的实例的类：闹钟类，日程事件类，
 
---
+++ app/src/main/java/com/example/myapplication/create_alarm_data_activity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class create_alarm_data_activity extends Activity {
+
+@Override
+    public void onCreate(Bundle savedInstanceState)
+{
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.create_ring_date);
+}
+
+}
