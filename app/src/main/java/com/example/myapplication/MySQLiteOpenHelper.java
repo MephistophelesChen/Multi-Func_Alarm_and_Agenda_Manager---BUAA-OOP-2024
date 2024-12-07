73ccc5f54c808353bd64e50438e7e31630c80928
Baiiduu <2698425919@qq.com>
2024-12-07 11:30:18 +0800

    12.7

--- app/src/main/java/com/example/myapplication/MySQLiteOpenHelper.java
+++ app/src/main/java/com/example/myapplication/MySQLiteOpenHelper.java
@@ -3,34 +3,37 @@ package com.example.myapplication;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 public class MySQLiteOpenHelper extends SQLiteOpenHelper{
-    private static final String db_name = "MySchedule";//自定义的数据库名；
-    private static final int version = 1;//版本号
+   private static final String DATABASE_NAME = "mydatabase.db";
+   private static final int DATABASE_VERSION=1;
+   private static final String TABLE_NAME = "mytable";
+   private static final String COLUMN_ID = "id";
 
-    public MySQLiteOpenHelper(Context context) {
-        super(context, db_name, null, version);
-    }
+   private static final String TABLE_DATE_ATTRIBUTE_CREATE =
+           "CREATE TABLE DateAttribute("+
+           "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+           "attribute1 TEXT,"+
+           "attribute2 TEXT);";
+   public static final String TABLE_LOCAL_DATE_MAP_CREATE=
+           "CREATE TABLE LocalDateMap("+
+           "date TEXT PRIMARY KEY,"+
+           "dateAttributeId INTEGER);";
 
-    // 该方法会自动调用，首先系统会检查该程序中是否存在数据库名为‘MySchedule’的数据库
-    // 如果存在则不会执行该方法，如果不存在则会执行该方法。
+
+   public MySQLiteOpenHelper(Context context){
+       super(context,DATABASE_NAME,null,DATABASE_VERSION);
+   }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String  sql ="create table schedules(" +
-                "id Integer primary key autoincrement," +     //id自增,只支持integer不支持int
-                "scheduleDetail varchar(50)," +
-                "time varchar(30)" +
-                ")";
-        db.execSQL(sql);
+        db.execSQL(TABLE_DATE_ATTRIBUTE_CREATE);
+        db.execSQL(TABLE_LOCAL_DATE_MAP_CREATE);
     }
 
-
-    //数据库版本更新时执行该方法,如果表已存在则先删除再调用onCreate重新创建
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists schedules");
-        onCreate(db);
+        db.execSQL("DROP TABLE IF EXISTS DateAttribute");
+        db.execSQL("DROP TABLE IF EXISTS LocalDateMap");
+       onCreate(db);
     }
-
 }
 
